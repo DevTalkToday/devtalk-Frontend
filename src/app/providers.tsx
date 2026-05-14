fc3561f243a72706ed9790c930fcfa976ca783ad
kangtaehyeon <s25002@gsm.hs.kr>
2026-05-14 17:19:25 +0900

    feat: add app shell and theme system

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,12 @@
 "use client";
 
+import { ThemeProvider } from "@/components/theme/theme-provider";
 import { QueryProvider } from "@/lib/query/provider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <QueryProvider>{children}</QueryProvider>;
+  return (
+    <ThemeProvider>
+      <QueryProvider>{children}</QueryProvider>
+    </ThemeProvider>
+  );
 }
